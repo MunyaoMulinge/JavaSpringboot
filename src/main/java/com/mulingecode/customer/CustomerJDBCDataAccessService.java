d0d657bd0ffd581ee311bdb0afd91973e982c948
Victor Munyao <victor.mulinge@tangazoletu.com>
2024-08-19 16:36:12 +0300

    updates

---
+++ src/main/java/com/mulingecode/customer/CustomerJDBCDataAccessService.java
@@ -0,0 +1,69 @@
+package com.mulingecode.customer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDataAccessService implements CustomerDao{
+
+    private final JdbcTemplate jdbcTemplate;
+    private final CustomerRowMapper customerRowMapper;
+
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate, CustomerRowMapper customerRowMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.customerRowMapper = customerRowMapper;
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        var sql = """
+                SELECT id, name, email,age
+                FROM customer
+                """;
+
+        return jdbcTemplate.query(sql, customerRowMapper);
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+        var sql = """
+                INSERT INTO customer(name, email, age)
+                VALUES (?, ?, ?)
+                """;
+        int result = jdbcTemplate.update(sql,
+                customer.getName(),
+                customer.getEmail(),
+                customer.getAge()
+        );
+        System.out.println("jdbcTemplate.update = " + result);
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existsPersonWithId(Integer id) {
+        return false;
+    }
+
+    @Override
+    public void deleteCustomerById(Integer customerId) {
+
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
+}
